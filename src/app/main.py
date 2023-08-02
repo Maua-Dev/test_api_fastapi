d1480c71b7a42df6745559743a00606316757dea
Vitor Soller <vgsoller@gmail.com>
2023-08-01 23:40:28 -0300

    add cors middle (será que a culpa é do fast api?)

--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from mangum import Mangum
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 
 @app.get("/")
 def read_root():
